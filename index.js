cc4d61c4940706135883b0946f65c6ea52e2bc1f
AnomolyAvery <ashawsolutions@gmail.com>
2022-03-23 16:47:35 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+
+const config = {
+    port: process.env.PORT || 3000,
+};
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(config.port, () => {
+    console.log(
+        `Server is listening on port ${config.port} (http://localhost:${config.port})`
+    );
+});
