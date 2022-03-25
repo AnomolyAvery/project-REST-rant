fe52e4784cdfd0ceff6d7da09e66440c27dc0e23
AnomolyAvery <ashawsolutions@gmail.com>
2022-03-25 15:14:30 -0400

    Added home page view

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const config = {
 
 const app = express();
 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 app.use('/places', require('./controllers/places'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('home');
 });
 
 app.get('*', (req, res) => {
