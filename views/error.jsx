7269171cf9a84d891da0124c3ac4123d8e883ccf
AnomolyAvery <ashawsolutions@gmail.com>
2022-03-26 11:07:11 -0400

    Added bootstrap grid to index page

--- views/error.jsx
+++ views/error.jsx
@@ -8,6 +8,17 @@ const Error = () => {
             <main>
                 <h1>404: Page not found</h1>
                 <p>Oops, sorry, we can't find that page!</p>
+                <img
+                    src="/images/bart-simpson-404.gif"
+                    alt="Bart Simpson Try Again"
+                />
+                <a href="https://tenor.com/view/bart-simpson-the-simpsons-at-least-you-tried-fail-failure-gif-7859548">
+                    Bart Simpson The Simpsons GIF
+                </a>
+                from{' '}
+                <a href="https://tenor.com/search/bart+simpson-gifs">
+                    Bart Simpson GIFs
+                </a>
             </main>
         </Def>
     );
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -5,14 +5,19 @@ const Index = ({ places }) => {
     return (
         <Def>
             <main>
-                <h1>Index page for places dir.</h1>
-
-                {places.map((place, i) => (
-                    <div key={i}>
-                        <h2>{place.name}</h2>
-                        <img src={place.pic} alt={place.name} />
-                    </div>
-                ))}
+                <h1>Places to Rant About.</h1>
+                <div className="row">
+                    {places.map((place, i) => (
+                        <div className="col-sm-6" key={i}>
+                            <h2>{place.name}</h2>
+                            <p>{place.cuisines}</p>
+                            <img src={place.pic} alt={place.name} />
+                            <p>
+                                Located in {place.city}, {place.state}
+                            </p>
+                        </div>
+                    ))}
+                </div>
             </main>
         </Def>
     );
