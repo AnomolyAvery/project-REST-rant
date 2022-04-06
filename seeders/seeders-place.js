63c92e34cda350cd96e0a09b7e105840cc38524c
Avery Shaw <averyshaw@Averys-MacBook-Air.local>
2022-04-06 18:11:55 -0400

    Added place seeder

---
+++ seeders/seeders-place.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const connectToDatbase = require('../db');
+const PlaceModel = require('../models/places');
+
+(async () => {
+    await connectToDatbase();
+    PlaceModel.create([
+        {
+            name: 'H-Thai-ML',
+            city: 'Seattle',
+            state: 'WA',
+            cuisines: 'Thai, Pan-Asian',
+            pic: '/images/h-thai-ml-tables.jpg',
+            founded: 1989,
+        },
+        {
+            name: 'Coding Cat Cafe',
+            city: 'Phoenix',
+            state: 'AZ',
+            cuisines: 'Coffee, Bakery',
+            pic: '/images/coffee-cat.jpg',
+            founded: 2020,
+        },
+    ])
+        .then(() => {
+            console.log('Success!');
+            process.exit();
+        })
+        .catch((err) => {
+            console.log('Failure!', err);
+            process.exit();
+        });
+})();
---
+++ views/show.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const Def = require('./default');
+
+const Show = () => {
+    return (
+        <Def>
+            <main>
+                <h2>Show page</h2>
+            </main>
+        </Def>
+    );
+};
+
+module.exports = Show;
